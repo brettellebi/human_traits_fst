3304ffa8527e936b0d692c06fe5a29722c04f447
brettellebi <brettell@hx-noah-32-01.ebi.ac.uk>
2021-01-14 15:38:25 +0000

    20210114 upload global allele freqs from cluster

---
+++ code/scripts/20210114_get_neg_control_snps.R
@@ -0,0 +1,8 @@
+#!/usr/bin/env Rscript
+
+# Bash script:
+# Rscript --vanilla hmn_fst/code/scripts/20210114_get_neg_control_snps.R <chromosome> <population> <output>
+
+args = commandArgs(trailingOnly = TRUE)
+
+# Import libraries
