801bc18c5eed3e44dbb5609e0f4589fe7dd2fb56
brettellebi <brettell@ebi.ac.uk>
2021-05-26 13:23:14 +0100

    20210526 update from codon 2

---
+++ code/snakemake/20210416/scripts/get_studies.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+# Libraries
+
+library(gwasrapidd)
+
+# Read in data
+
+assocs = readRDS(snakemake@input[[1]])
+
+# Get linked studies
+
+studies_key = gwasrapidd::association_to_study(unique(assocs@associations$association_id))
+
+# Get study data
+
+studies = gwasrapidd::get_studies(study_id = unique(studies_key$study_id))
+
+# Save to file
+
+saveRDS(studies, snakemake@output[[1]])
